cdd1f6211db507e524f904605cdcac80a8182387
kotkem <louishowk9@gmail.com>
2025-11-21 14:54:27 +0000

    creacion de archivos de texto

---
+++ numeros.py
@@ -0,0 +1,10 @@
+entero = 10
+decimal = 1.0
+modulo = entero % 2
+suma = entero + 5
+
+print("Suma :", suma)
+print("Decimal :", decimal)
+print("DIvision :", ((entero / 3) +3))
+print("Potencia ", (entero ** 2))
+print("Modulo: ", (modulo))
